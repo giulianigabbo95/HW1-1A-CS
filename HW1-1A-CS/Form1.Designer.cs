5619aece3157c67956a7bd59ba9a58f4eb4179cf
vipaak <amazingvipaak@gmail.com>
2022-02-07 18:27:55 +0100

    Primo Commit
    La pallina segue sempre lo stesso percorso. Una volta fatta partire l'animazione non si può fermare o premere nessun altro tasto.

---
+++ HW1-1A-CS/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace HW1
+{
+    partial class Finestra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bottoneTesto = new System.Windows.Forms.Button();
+            this.spazioTesto = new System.Windows.Forms.RichTextBox();
+            this.bottonePalla = new System.Windows.Forms.Button();
+            this.rettangoloPalla = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // bottoneTesto
+            // 
+            this.bottoneTesto.Location = new System.Drawing.Point(13, 13);
+            this.bottoneTesto.Name = "bottoneTesto";
+            this.bottoneTesto.Size = new System.Drawing.Size(134, 23);
+            this.bottoneTesto.TabIndex = 0;
+            this.bottoneTesto.Text = "Stampa Testo";
+            this.bottoneTesto.UseVisualStyleBackColor = true;
+            this.bottoneTesto.Click += new System.EventHandler(this.bottoneTesto_Click);
+            // 
+            // spazioTesto
+            // 
+            this.spazioTesto.Location = new System.Drawing.Point(13, 43);
+            this.spazioTesto.Name = "spazioTesto";
+            this.spazioTesto.Size = new System.Drawing.Size(134, 54);
+            this.spazioTesto.TabIndex = 1;
+            this.spazioTesto.Text = "";
+            // 
+            // bottonePalla
+            // 
+            this.bottonePalla.Location = new System.Drawing.Point(12, 154);
+            this.bottonePalla.Name = "bottonePalla";
+            this.bottonePalla.Size = new System.Drawing.Size(134, 23);
+            this.bottonePalla.TabIndex = 3;
+            this.bottonePalla.Text = "Anima Palla";
+            this.bottonePalla.UseVisualStyleBackColor = true;
+            this.bottonePalla.Click += new System.EventHandler(this.bottonePalle_Click);
+            // 
+            // rettangoloPalla
+            // 
+            this.rettangoloPalla.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rettangoloPalla.Location = new System.Drawing.Point(12, 183);
+            this.rettangoloPalla.Name = "rettangoloPalla";
+            this.rettangoloPalla.Size = new System.Drawing.Size(238, 255);
+            this.rettangoloPalla.TabIndex = 4;
+            this.rettangoloPalla.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintCircle);
+            // 
+            // Finestra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 450);
+            this.Controls.Add(this.rettangoloPalla);
+            this.Controls.Add(this.bottonePalla);
+            this.Controls.Add(this.spazioTesto);
+            this.Controls.Add(this.bottoneTesto);
+            this.Name = "Finestra";
+            this.Text = "Finestra";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bottoneTesto;
+        private System.Windows.Forms.RichTextBox spazioTesto;
+        private System.Windows.Forms.Button bottonePalla;
+        private System.Windows.Forms.Panel rettangoloPalla;
+    }
+}
+
---
+++ HW1-1A-CS/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace HW1
+{
+    public partial class Finestra : Form
+    {
+        // Inizializzo le variabili, le prime due definiscono la dimensione della palla, le successive la sua posizione e le ultime di quanto deve spostarsi
+        private int ballWidth = 50;
+        private int ballHeight = 50;
+        private int ballPosX = 100;
+        private int ballPosY = 100;
+        private int moveStepX = 1;
+        private int moveStepY = 1;
+
+        public Finestra()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+            this.UpdateStyles();
+        }
+
+        // Questo metodo fa apparire il testo nello spazio designato quando viene premuto l'apposito tasto
+        private void bottoneTesto_Click(object sender, EventArgs e)
+        {
+            spazioTesto.Text = "qwertyuiop\nasdfghjkl\nzxcvbnm";
+        }
+
+        // Questo metodo fa apparire la palla nel rettangolo e la fa muovere quando viene premuto l'apposito tasto
+        // Per evitare che sia troppo veloce ogni volta che fa un movimento si ferma per 20 millisecondi
+        private void bottonePalle_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 500; i++)
+            {
+                MoveBall(sender, e);
+                spazioTesto.Refresh();
+                Thread.Sleep(20);
+            }
+        }
+
+        //  Questo metodo cambia la posizione della palla nel rettangolo
+        private void MoveBall(object sender, EventArgs e)
+        {
+            ballPosX += moveStepX;
+            if (ballPosX < 0 || ballPosX + ballWidth > rettangoloPalla.ClientSize.Width)
+            {
+                moveStepX = -moveStepX;
+            }
+            ballPosY += moveStepY;
+            if (ballPosY < 0 || ballPosY + ballHeight > rettangoloPalla.ClientSize.Height)
+            {
+                moveStepY = -moveStepY;
+            }
+            rettangoloPalla.Refresh();
+        }
+
+        // Questo è un metodo che disegma il cerchio nel rettangolo
+        private void PaintCircle(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(rettangoloPalla.BackColor);
+            e.Graphics.FillEllipse(Brushes.Green, ballPosX, ballPosY, ballWidth, ballHeight);
+        }
+    }
+}
